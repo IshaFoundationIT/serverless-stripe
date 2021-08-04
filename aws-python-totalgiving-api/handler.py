2d0cc5a42bacc0e183bc2152ed3ec0883579b860
Kirthi Siva <contact@kirthisiva.in>
2021-08-04 05:19:15 +0000

    adding eventbridge - WIP

--- aws-python-totalgiving-api/handler.py
+++ aws-python-totalgiving-api/handler.py
@@ -1,64 +1,32 @@
+import odoorpc
 import json
-import stripe
 
 def totalgiving(event, context):
     print(event)
     print(context)
-    stripe.api_key = "sk_test_51H05s0FOdz1GkABXzM29YYc6GfuIItqnHbInBpP9iB7CaaJYLJW2IQy7TnY1pxgN3oWBKGAt9QbxTYSm0DY72Neb00NGyAeWqX"
-#     requestContextStage = event.requestContext
-#     ? event.requestContext.stage
-#     : 'test';
-#     console.log(event.requestContext.stage)
-#   const stripeApiKey =
-#     requestContextStage === 'test'
-#     ? ConfigFile.stripe.test_sk
-#     : ConfigFile.stripe.live_sk;
-#   console.log(event)
-#   const stripe = require('stripe')(stripeApiKey); // eslint-disable-line
-#   const endpointSecret = 'whsec_...';
-#   const app = require('express')();
-#   const bodyParser = require('body-parser');
-#   try {
-#     // Parse Stripe Event
-#     console.log(event.body.id)
-#     const jsonData = event
-#     // const jsonData = JSON.parse(event); // https://stripe.com/docs/api#event_object
-#     console.log("got json data")
-#     // Verify the event by fetching it from Stripe
-#     console.log("Stripe Event: %j", jsonData); // eslint-disable-line
-    
-#     const response = {
-#         statusCode: 200,
-#         body: JSON.stringify({
-#           message: 'Stripe webhook incoming!',
-#           stage: requestContextStage,
-#         }),
-#       };
-#       callback(null, response);
-# }
-#   catch (err) {
-#     callback(null, {
-#       statusCode: err.statusCode || 501,
-#       headers: { 'Content-Type': 'text/plain' },
-#       body: err.message || err.message,
-#     });
-#   }
-# };
+    model = 'donation.donation'
+    data = event['body']
+    print(data)
+    username = "admin"
+    password = "admin"
 
-#     body = {
-#         "message": "Go Serverless v2.0! Your function executed successfully!",
-#         "input": event,
-#     }
+    url = "8069-amber-porpoise-u4ivsce1.ws-us13.gitpod.io"
+    url = "uk.sulaba.isha.us"
+    db = "ishafoundationit-odoo14-master-2820710"
 
-    response = {"statusCode": 200, "body": json.dumps(event)}
+    odoo = odoorpc.ODOO(url,protocol='jsonrpc+ssl',port='443')
+    print("++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("initialzied")
+    odoo.login(db, username, password)
+    print("logged in")
+    user = odoo.env.user
+    print(user)
 
+    try:
+        id = odoo.env['donation.donation'].create(eval(data))
+        print("id created is {0}".format(id))
+        response = {"statusCode": 200, "id": id}
+    except Exception as e:
+        response = {"statusCode": 501, "error": str(e)}
+    print("response is {0}".format(response))
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
