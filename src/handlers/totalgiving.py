293b3e2d886f3fa5705f8c5237e9595ef673d699
Sufyan <soufiane.felliaui@ishafoundation.org>
2021-08-24 14:29:12 +0530

    add schema validation of incoming web hook data

--- src/handlers/totalgiving.py
+++ src/handlers/totalgiving.py
@@ -1,11 +1,19 @@
 import os
+import json
 import stripe
-import boto
+import boto3 as boto
+import logging
 from requests.structures import CaseInsensitiveDict
+from schema import Schema, And, Use, Optional, SchemaError
 
-QUEUE_NAME = os.environ["QUEUE_NAME"]
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-BUS = boto.client("sqs")
+
+QUEUE_URL = os.environ["QUEUE"]
+
+QUEUE = boto.client("sqs")
+
 
 def respond(code=200, body="", headers={}):
     return {
@@ -15,6 +23,67 @@ def respond(code=200, body="", headers={}):
     }
 
 
-def charge_succeded(event, context):
+webhook_payload_schema = Schema({
+    'id': And(str, len),
+    'object': 'donation',
+    "created": str,
+    'event': str,
+    "donation": {
+        "id": str,
+        "amount": str,
+        "currency": str,
+        "exchangerate": str,
+        "datetime": str,
+        "giftaid": str,
+        "displayname": str,
+        "message": str,
+        "repeat": str,
+        "recurrence": str,
+        "recurrenceno": str,
+        "cancelled": str,
+        "custom": str,
+        },
+    "supporter": {
+        "id": str,
+        "title": str,
+        "firstname": str,
+        "surname": str,
+        "address1": str,
+        "address2": str,
+        "town": str,
+        "postcode": str,
+        "county": str,
+        "country": str,
+        "telephone": str,
+        "fax": str,
+        "mobile": str,
+        "email": str,
+        "mailinglist": str,
+        "active": str,
+        }
+    })
+
+
+
+
+def validate_webhook_payload(payload):
+    pass
+
+def donation_succeded(event, context):
     headers, body = CaseInsensitiveDict(event["headers"]), json.loads(event["body"])
+    logger.info(headers)
+    logger.info(body)
+
+    try:
+        webhook_payload_schema.validate(body)
+    except SchemaError as e:
+        logger.warning("Bad payload")
+        logger.warning(e)
+        return respond(400, {"message": "Bad payload", "error": str(e)}) # XXX: only get the errors
+
+    response = QUEUE.send_message(QueueUrl=QUEUE_URL,
+                        MessageBody=event['body'])
+
+    logger.info(response)
+
     return respond(200)
